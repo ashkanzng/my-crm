05a5e14e7300073a9aad806581119f7c7a169d14
Ashkan-Zanjani <ash333d@gmail.com>
2021-08-22 22:57:25 +0400

    Should fix UI

---
+++ src/main/java/com/api/crm/api/ApiClient.java
@@ -0,0 +1,82 @@
+package com.api.crm.api;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import java.util.*;
+
+public class ApiClient {
+
+    private final static Logger logger = LoggerFactory.getLogger(ApiClient.class);
+    static final String URL = "http://localhost:8082/api/";
+    static final RestTemplate restTemplate = new RestTemplate();
+    static ObjectMapper mapper = new ObjectMapper();
+
+
+    public static String[] getAllTables() {
+        ResponseEntity<String[]> response = restTemplate.getForEntity(URL + "get-tables", String[].class);
+        return response.getBody();
+    }
+
+    public static String[] getTableSchema(String tableName) {
+        ResponseEntity<String[]> response = restTemplate.getForEntity(URL + "get-table-schema/" + tableName, String[].class);
+        return response.getBody();
+    }
+
+    public static void createTable(String tableName, Set<String> columns) {
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            Map<String, String> tableNameRequest = new HashMap<>() {{
+                put("name", tableName);
+            }};
+            String[] tableColumnRequest = Arrays.copyOf(columns.toArray(), columns.size(), String[].class);
+            Arrays.sort(tableColumnRequest);
+            HttpEntity<String> request = new HttpEntity<>(mapper.writeValueAsString(tableNameRequest), headers);
+            ResponseEntity<String> result = restTemplate.postForEntity(URL + "create-table", request, String.class);
+            request = new HttpEntity<>(mapper.writeValueAsString(tableColumnRequest), headers);
+            result = restTemplate.postForEntity(URL + "add-header/" + tableName, request, String.class);
+            if (result.getStatusCode() == HttpStatus.OK) {
+                logger.info(result.getBody());
+            }
+        } catch (JsonProcessingException jsonProcessingException) {
+            logger.error(jsonProcessingException.getMessage());
+        }
+    }
+
+    public static List<Map<String, String>> getTableData(String tableName) {
+        try {
+            //ResponseEntity<?extends List<Map<String,String>>> result = restTemplate.getForEntity(URL+"get-all-data/"+tableName, (Class<?  extends List<Map<String,String>>>) List.class);
+            ResponseEntity<List<Map<String, String>>> result = restTemplate.exchange(URL + "get-all-data/" + tableName, HttpMethod.GET, null, new ParameterizedTypeReference<>() {
+            });
+            if (result.getStatusCode() == HttpStatus.OK) {
+                return result.getBody();
+            }
+        } catch (HttpClientErrorException httpClientErrorException) {
+            logger.error(httpClientErrorException.getMessage());
+        }
+        return null;
+    }
+
+    public static void addTableData(String tableName, List<Map<String, String>> data, Optional<Integer> id){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        try{
+            HttpEntity<String> request = new HttpEntity<>(mapper.writeValueAsString(data.get(0)), headers);
+            if (id.isPresent()){
+                restTemplate.postForEntity(URL + "update/" + tableName+"/"+id.get(), request, String.class);
+                return;
+            }
+            request = new HttpEntity<>(mapper.writeValueAsString(data), headers);
+            restTemplate.postForEntity(URL + "add-data/" + tableName, request, String.class);
+        }catch (HttpClientErrorException | JsonProcessingException ex ){
+            logger.info(ex.getMessage());
+        }
+    }
+}
--- src/main/java/com/api/crm/view/HomeView.java
+++ src/main/java/com/api/crm/view/HomeView.java
@@ -1,8 +1,12 @@
 package com.api.crm.view;
 
+import com.api.crm.api.ApiClient;
 import com.api.crm.component.LayoutComponent;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.listbox.ListBox;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -10,10 +14,7 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
-
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @PageTitle("Home")
 @Route(value = "home", layout = MainLayout.class)
@@ -35,18 +36,17 @@ public class HomeView extends HorizontalLayout {
 
     public HomeView() {
         initializing();
-//        createTableForm();
-//        showTables();
+        createTableForm();
+        showTables();
         add(layoutComponent.getMainLayout(),layoutComponent.getGridLayout());
     }
 
     private void initializing(){
         newTableColumns = new HashSet<>();
-        //allTables = ApiClient.getAllTables();
-
+        allTables = ApiClient.getAllTables();
         layoutComponent = new LayoutComponent();
         grid = new Grid<>();
-        //grid.addItemClickListener(e -> fillDataForm(e.getItem()));
+        grid.addItemClickListener(e -> fillDataForm(e.getItem()));
         grid.setMaxHeight("450px");
         layoutComponent.getGridLayout().add(grid);
         dataForm = new FormLayout();
@@ -64,6 +64,179 @@ public class HomeView extends HorizontalLayout {
         );
         tableName = new TextField("Table name");
         columnName = new TextField("Column name");
+    }
 
+    public void showTables(){
+        listBox = new ListBox<>();
+        if (allTables.length > 0){
+            listBox.setItems(allTables);
+            listBox.setValue(allTables[0]);
+            showTableColumnsAndGrid(allTables[0]);
+        }
+        listBox.addValueChangeListener(e->{
+            if (e.getValue() == null) return;
+            resetForm();
+            showTableColumnsAndGrid(e.getValue());
+        });
+        layoutComponent.getListBoxLayout().add(listBox);
+        layoutComponent.getMainLayout().addToPrimary(layoutComponent.getListBoxLayout());
+    }
+
+    private void createTableForm() {
+        columnLayout = new VerticalLayout();
+        operationLayout = new HorizontalLayout();
+        HorizontalLayout buttonLayout = new HorizontalLayout();
+        columnLayout.setPadding(false);
+        columnLayout.add(columnForm);
+        buttonLayout.setDefaultVerticalComponentAlignment(Alignment.END);
+        tableName.setRequired(true);
+        tableName.setWidth("200px");
+        tableName.addKeyDownListener(v -> {
+            if (tableName.hasClassName("error")) tableName.removeClassName("error");
+        });
+        columnName.setWidth("160px");
+        columnName.setRequired(true);
+        Button save = new Button("Save", VaadinIcon.FILE_ADD.create(), e -> {
+            ApiClient.createTable(tableName.getValue(), newTableColumns);
+            clearForm();
+            allTables = ApiClient.getAllTables();
+            listBox.setItems(allTables);
+        });
+        Button cancel = new Button("Cancel", VaadinIcon.CLOSE_CIRCLE.create(),e -> {
+            clearForm();
+        });
+        Button addColumnButton = new Button("", VaadinIcon.PLUS.create(),e -> {
+            addColumn(columnName);
+        });
+        addColumnButton.setMaxWidth("120px");
+        buttonLayout.add(tableName, columnName, addColumnButton);
+        operationLayout.add(cancel,save);
+        operationLayout.setVisible(false);
+        layoutComponent.getFormLayout().add(buttonLayout,columnLayout,operationLayout);
+        layoutComponent.getSecondLayout().addToPrimary(layoutComponent.getFormLayout());
+    }
+
+    private void addColumn(TextField columnName){
+        if (tableName.getValue().isEmpty()) {
+            //tableName.setErrorMessage("fill the name");
+            //tableName.setInvalid(true);
+            tableName.setClassName("error");
+            return;
+        }
+        if (columnName.getValue().isEmpty()){
+            columnName.setClassName("error");
+            return;
+        }
+        if (!newTableColumns.contains(columnName.getValue())){
+            TextField newColumn = new TextField();
+            newColumn.setValue(columnName.getValue());
+            newColumn.setReadOnly(true);
+            newColumn.setMaxWidth("200px");
+            newTableColumns.add(columnName.getValue());
+            columnForm.add(newColumn);
+        }
+        if (!operationLayout.isVisible()) operationLayout.setVisible(true);
+    }
+
+    private void createTableGrid(String tableName){
+        List<Map<String, String>> data = ApiClient.getTableData(tableName);
+        if (data == null){
+            data = List.of(Map.of("id",""));
+        }
+        String[] headers = ApiClient.getTableSchema(tableName);
+        for (String column : headers) {
+            grid.addColumn(col -> {
+                if (col.get(column) == null || col.get(column).isEmpty()){
+                    return "-";
+                }
+                return col.get(column);
+            }).setHeader(column).setSortable(true).setAutoWidth(true);
+        }
+        //grid.addColumn(myhash -> myhash.get("id")).setHeader("Id").setSortable(true);
+        grid.setItems(data);
+        createDataForm(headers);
+    }
+
+    private void createDataForm(String[] headers){
+        dataForm.removeAll();
+        HorizontalLayout buttonLayout = new HorizontalLayout();
+        dataFormLayout = new VerticalLayout(new Label("Insert/Update data"));
+        buttonLayout.setDefaultVerticalComponentAlignment(Alignment.END);
+        Button cancel = new Button("Cancel", VaadinIcon.CLOSE_CIRCLE.create(),e -> {
+            clearForm();
+        });
+        Button save = new Button("Save", VaadinIcon.FILE_ADD.create(), e -> {
+            saveDataForm();
+            grid.removeAllColumns();
+            createTableGrid(listBox.getValue());
+        });
+        for (String header : headers) {
+            TextField field = new TextField(header);
+            if (header.equals("id") || header.equals("created_at") || header.equals("update_at")){
+                continue;
+            }
+            field.getElement().setProperty("name",header);
+            dataForm.add(field);
+        }
+        HorizontalLayout operationLayout = new HorizontalLayout(save,cancel);
+        dataFormLayout.add(dataForm,operationLayout);
+        layoutComponent.getSecondLayout().addToSecondary(dataFormLayout);
+    }
+
+    private void fillDataForm(Map<String, String> row){
+        TextField id = new TextField("ID",row.get("id"),"");
+        id.getElement().setProperty("name","id");
+        id.setReadOnly(true);
+        dataForm.getChildren().forEach(field -> {
+            if (field instanceof TextField && row.get(field.getElement().getProperty("name")) != null){
+                TextField textField = (TextField) field;
+                if (textField.getElement().getProperty("name").equals("id")) dataForm.remove(textField);
+                textField.setValue(row.get(field.getElement().getProperty("name")));
+            }
+        });
+        dataForm.addComponentAtIndex(0,id);
+    }
+
+    private void saveDataForm(){
+        List<Map<String, String>> dataList = new ArrayList<>();
+        Map<String, String> data = new HashMap<>();
+        dataForm.getChildren().forEach(field -> {
+            if (field instanceof TextField){
+                TextField textField = (TextField)field;
+                data.put(textField.getElement().getProperty("name"),textField.getValue());
+            }
+        });
+        dataList.add(data);
+        if (data.get("id") == null){
+            ApiClient.addTableData(listBox.getValue(),dataList, Optional.empty());
+            return;
+        }
+        Optional<Integer> id = Optional.of(Integer.parseInt(data.get("id")));
+        data.remove("id");
+        ApiClient.addTableData(listBox.getValue(),dataList,id);
+    }
+
+    private void showTableColumnsAndGrid(String nameOfTable){
+        tableName.setValue(nameOfTable);
+        operationLayout.setVisible(true);
+        for (String column : ApiClient.getTableSchema(nameOfTable)) {
+            addColumn(new TextField("",column,""));
+        }
+        createTableGrid(nameOfTable);
+    }
+
+    private void clearForm(){
+        resetForm();
+        listBox.setValue(null);
+    }
+
+    private void resetForm(){
+        operationLayout.setVisible(false);
+        tableName.clear();
+        columnName.clear();
+        columnForm.removeAll();
+        newTableColumns.clear();
+        grid.removeAllColumns();
+        dataFormLayout.removeAll();
     }
 }
